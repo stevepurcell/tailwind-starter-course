e85fa7b13e1595e47ab50b36d7596cd4d1de37f3
Steve Purcell <smp103@gmail.com>
2023-01-28 15:58:29 -0600

    fully working

--- plugins/openVariant.js
+++ plugins/openVariant.js
@@ -1,4 +1,4 @@
-const plugin = require("tailwindcss/plugin") 
+const plugin = require("tailwindcss/plugin");
 
 const openVariant = plugin(function({addVariant}) {
     addVariant("group-open", ":merge(.group).open &");
